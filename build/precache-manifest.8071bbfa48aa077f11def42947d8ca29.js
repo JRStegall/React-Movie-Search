8e099b6874ce526aefa4804fe09a45faa7cb15c0
Jeremy <jeremy.stegall77@gmail.com>
2020-11-18 13:59:41 -0500

    Movie Search Commit

---
+++ build/precache-manifest.8071bbfa48aa077f11def42947d8ca29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22dbb75a0f970c42c42824c663b0f8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62de850ea7505efb7eba",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cede773d01add826f5be",
+    "url": "/static/css/main.e8da3ed3.chunk.css"
+  },
+  {
+    "revision": "62de850ea7505efb7eba",
+    "url": "/static/js/2.f9d5545d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f9d5545d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cede773d01add826f5be",
+    "url": "/static/js/main.394d9255.chunk.js"
+  },
+  {
+    "revision": "5b4060d1d5e784799cd4",
+    "url": "/static/js/runtime-main.b85f6ef5.js"
+  }
+]);
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Header (){
+    return (
+        <div className="header">
+            <h1>Movie Search</h1>
+        </div>
+    )
+}
+
+export default Header;
